a2a3d2d369360d9d1b321581a44716161769bff3
Viacheslav Dubovskyi <architect1slava@gmail.com>
2022-08-14 15:54:32 +0300

    import lombok

--- src/main/java/com/example/demo/Book.java
+++ src/main/java/com/example/demo/Book.java
@@ -1,5 +1,11 @@
 package com.example.demo;
 
+import lombok.*;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Book {
 
     private int id;
@@ -7,8 +13,8 @@ public class Book {
     private String author;
     private String year;
 
-    public Book() {
-    }
+//    public Book() {
+//    }
 
     public Book(String title, String author, String year) {
         this.title = title;
@@ -16,37 +22,37 @@ public class Book {
         this.year = year;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getAuthor() {
+//        return author;
+//    }
+//
+//    public void setAuthor(String author) {
+//        this.author = author;
+//    }
+//
+//    public String getYear() {
+//        return year;
+//    }
+//
+//    public void setYear(String year) {
+//        this.year = year;
+//    }
 
     @Override
     public String toString() {
