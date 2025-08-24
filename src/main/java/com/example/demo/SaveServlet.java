622bd743179d2bd78bc02bb3a7ab2c420a44d9eb
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-08-24 11:41:47 +0300

    refactor save servlet

--- src/main/java/com/example/demo/SaveServlet.java
+++ src/main/java/com/example/demo/SaveServlet.java
@@ -8,8 +8,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.PrintWriter;
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @WebServlet("/saveServlet")
@@ -28,16 +26,6 @@ public class SaveServlet extends HttpServlet {
         printStatus(book, status, out);
     }
 
-    public Map<Integer, Book> putBookToMap(Book book) {
-        Map<Integer, Book> bookMap = new HashMap<>();
-        int bookID = book.getId();
-        String bookTitle = book.getTitle();
-        String bookAuthor = book.getAuthor();
-        String bookYear = book.getYear();
-        bookMap.put(bookID, new Book(bookTitle, bookAuthor, bookYear));
-        return bookMap;
-    }
-
     @Logged
     private void printStatus(Book book, int status, PrintWriter out) {
         if (status > 0) {
@@ -45,7 +33,7 @@ public class SaveServlet extends HttpServlet {
             out.print("Title: " + book.getTitle() + '\n');
             out.print("Author: " + book.getAuthor() + '\n');
             out.print("Year: " + book.getYear() + '\n');
-            log.info("save() - end: " + book);
+            log.info("save() - end: {}", book);
         } else {
             log.info("Unable to save record");
             out.println("Sorry! unable to save record");
