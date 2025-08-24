282c8f7759d764c379bef50050bdb4c0b1e595e7
Viacheslav Dubovskyi <architect1slava@gmail.com>
2025-08-24 11:52:15 +0300

    refactor delete servlet

--- src/main/java/com/example/demo/DeleteServlet.java
+++ src/main/java/com/example/demo/DeleteServlet.java
@@ -7,10 +7,9 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 import java.io.PrintWriter;
-
-import static java.lang.System.out;
 
 @Slf4j
 @WebServlet("/deleteServlet")
@@ -25,18 +24,15 @@ public class DeleteServlet extends HttpServlet {
 
     @Logged
     private void printDeletingMessage(HttpServletResponse response, int id) {
-        try {
+        try (PrintWriter out = response.getWriter()) {
             response.setContentType("text/html");
-            PrintWriter out;
-            out = response.getWriter();
+
             out.println("Deleting book by ID " + id + " was successful");
-            log.info("delete() - end: book with ID " + id + " was deleted");
+            log.info("delete() - end: book with ID {} was deleted", id);
 
         } catch (IOException e) {
             log.info("Unable to delete record. IOException has appear");
             throw new RuntimeException(e);
-        } finally {
-            out.close();
         }
     }
 }
